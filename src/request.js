d1557e1875c7f055b4ba52d7f4a0871d75fef494
Matt Collins <mailmattcollins@gmail.com>
2013-08-07 17:00:17 +0100

    Fixes isntances where #comments is in url.

--- src/request.js
+++ src/request.js
@@ -8,9 +8,15 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 
 function detectRedirect(details) {
 
-    if(!!details.url.match('http://www.theguardian.com') && !details.url.match("\\?view=mobile")){
-       return{
+    if(!!details.url.match('http://www.theguardian.com') && !details.url.match("\\?view=mobile") && !details.url.match("#comments")){
+        return{
             redirectUrl : details.url + '?view=mobile'
-       };
-    }
+        };
+    }else if(!!details.url.match('http://www.theguardian.com') && !details.url.match("\\?view=mobile") && !!details.url.match("#comments")){
+
+        var url =  details.url.replace('#comments', '');
+        return{
+            redirectUrl : url + '?view=mobile#comments'
+        };
+   }
 }
